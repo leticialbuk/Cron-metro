6bc1c4e9d441795a4e69f199528c75911c1bb5bb
leticialbuk <leticia.albuk@gmail.com>
2023-03-16 15:49:44 -0300

    Stopwatch

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
